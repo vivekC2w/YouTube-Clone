d72da5b88b3844593224dca81a5af77a82a0199d
vivekC2w <68582416+vivekC2w@users.noreply.github.com>
2023-09-27 12:21:49 +0530

    Update VideoDetails.jsx

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -81,7 +81,7 @@ const VideoDetails = () => {
             <div className="flex text-white mt-4 md:mt-0">
               <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
                 <AiOutlineLike className="text-xl text-white mr-2" />
-                {`${abbreviateNumber(video?.stats?.views, 2)} Likes`}
+                {`${abbreviateNumber(video?.stats?.likes, 2)} Likes`}
               </div>
               <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15] ml-4">
                 {`${abbreviateNumber(video?.stats?.views, 2)} Views`}
