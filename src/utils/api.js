e9c636c1af8f31282c9f3c6c01a38c7d79923aa4
vivekC2w <68582416+vivekC2w@users.noreply.github.com>
2023-10-08 14:54:37 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,14 @@
-import axios from "axios";
+const axios = require("axios");
 
 const BASE_URL = "https://youtube138.p.rapidapi.com";
 
 const options = {
-  params: { hl: "en", gl: "US" },
+  params: {
+    hl: "en",
+    gl: "US",
+  },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY,
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
